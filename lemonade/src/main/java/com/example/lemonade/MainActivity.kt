c7fec704cd23e5a8f2546d51e990f87c290ccee4
deandoff <deandoff@gmail.com>
2025-09-30 18:42:33 +0300

    u2_p2

---
+++ lemonade/src/main/java/com/example/lemonade/MainActivity.kt
@@ -0,0 +1,150 @@
+package com.example.lemonade
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LemonApp()
+        }
+    }
+}
+
+@Composable
+fun LemonApp() {
+    var step by remember { mutableIntStateOf(1) }
+    var squeezeCount by remember { mutableIntStateOf(0) }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Header()
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .clickable {
+                    when (step) {
+                        1 -> {
+                            squeezeCount = (2..6).random()
+                            step = 2
+                        }
+                        2 -> {
+                            squeezeCount--
+                            if (squeezeCount == 0) {
+                                step = 3
+                            }
+                        }
+                        3 -> step = 4
+                        4 -> step = 1
+                    }
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            when (step) {
+                1 -> LemonStep(R.drawable.lemon_tree, R.string.lemon_tree)
+                2 -> LemonStep(R.drawable.lemon_squeeze, R.string.lemon)
+                3 -> LemonStep(R.drawable.lemon_drink, R.string.glass_of_lemonade)
+                4 -> LemonStep(R.drawable.lemon_restart, R.string.empty_glass)
+            }
+        }
+    }
+}
+
+@Composable
+fun Header() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .background(Color(0xfff9e44c)),
+        contentAlignment = Alignment.BottomCenter
+
+    ) {
+        Text(
+            text = "Lemonade",
+            fontSize = 20.sp,
+            color = Color.Black,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 12.dp)
+        )
+    }
+}
+
+@Composable
+fun LemonStep(
+    imageResId: Int,
+    textResId: Int,
+    modifier: Modifier = Modifier
+) {
+    Column (
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(imageResId),
+            contentDescription = stringResource(textResId),
+            modifier = Modifier
+                .border(
+                    width = 2.dp,
+                    color = Color(195, 236, 210),
+                    shape = androidx.compose.foundation.shape.RoundedCornerShape(4.dp)
+                )
+                .background(
+                    Color(195, 236, 210),
+                    shape = androidx.compose.foundation.shape.RoundedCornerShape(4.dp)
+                )
+                .size(200.dp)
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(
+            text = stringResource(textResId),
+            style = TextStyle(
+                fontSize = 18.sp
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LemonApp()
+}
